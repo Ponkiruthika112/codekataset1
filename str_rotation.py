bae1c7e2a7a900202974a957522a5a14e3e9457b
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-02-10 22:15:59 +0800

    Update str_rotation.py

--- str_rotation.py
+++ str_rotation.py
@@ -2,7 +2,10 @@ s,k=map(str,input().split())
 j=int(k)
 g=j%len(s)
 l=""
-l=l+s[-g:len(s)]
-l=l+s[0:len(s)-g]
+if g!=0:
+    l=l+s[-g:len(s)]
+    l=l+s[0:len(s)-g]
+else:
+    l=s
 print(l)
-#rotation
+#string rotation
