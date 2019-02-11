9bbe5942c2a123b79ad9c683e0235dd97a15a84c
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-02-11 22:59:27 +0800

    Create print_bef_0.py

---
+++ print_bef_0.py
@@ -0,0 +1,18 @@
+n=int(input())
+l=list(map(int,input().split()))
+s=""
+for i in range(0,len(l)):
+    s=s+str(l[i])
+k=""
+i=0
+while i<len(s):
+    if s[i]=="0" and s[i-1]=="0":
+        i=i+1
+    elif s[i]=="0":
+        k=s[:i]
+        i=i+1
+    else:
+        i=i+1
+print(k)
+        
+#0 nd 1
