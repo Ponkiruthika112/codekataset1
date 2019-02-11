db168868e2b19f186aa88884134b32cc247e0f65
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-02-11 15:21:39 +0530

    Update sub_str.py

--- sub_str.py
+++ sub_str.py
@@ -2,9 +2,9 @@ s=input()
 c=0
 k=0
 for i in range(0,len(s)-5):
-    if s[i:i+6]=="sundar":
+    if s[i:i+6]=="Sundar":
         c=c+1
-    if s[i:i+6]=="vishal":
+    if s[i:i+6]=="Vishal":
         k=k+1
 if c!=0 and k!=0:
     print("yes")
