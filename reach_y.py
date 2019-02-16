12e3edd3d055f03065cffccd97c6a7156fd4f035
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-02-16 16:49:59 +0800

    Update reach_y.py

--- reach_y.py
+++ reach_y.py
@@ -1,11 +1,14 @@
 n,k=map(int,input().split())
 l=[]
+c=0
 for i in range(0,n):
     l.append(list(map(int,input().split())))
 for i in range(0,len(l)):
     if l[i][1]==k:
-        print("YES")
-    else:
-        print("NO")
+        c=c+1
+if c==0:
+    print("no")
+else:
+    print("yes")
     
-#reach
+#fghjk
