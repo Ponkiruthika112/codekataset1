8506e9129075c78dd570e340be417881a57f6737
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-02-17 16:12:56 +0800

    Create xor_range.py

---
+++ xor_range.py
@@ -0,0 +1,9 @@
+n,k=map(int,input().split())
+l=list(map(int,input().split()))
+for i in range(0,k):
+    a,b=map(int,input().split())
+    s=0
+    for i in range(a-1,b):
+        s=s^l[i]
+    print(s)
+#xor range
