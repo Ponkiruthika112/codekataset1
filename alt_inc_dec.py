dae1c812ed4f11bd6a5d34ca479abe85f3e08105
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-03-14 21:26:51 +0800

    Create alt_inc_dec.py

---
+++ alt_inc_dec.py
@@ -0,0 +1,14 @@
+n=int(input())
+a=list(map(int,input().split()))
+c=0
+k=0
+for i in range(0,len(a)-2):
+    if a[i]<a[i+1] and a[i+1]>a[i+2]:
+        c=c+1
+    elif a[i]>a[i+1] and a[i+1]<a[i+2]:
+        k=k+1
+if c+k==len(a)-2:
+    print("yes")
+else:
+    print("no")
+alter inc dec
