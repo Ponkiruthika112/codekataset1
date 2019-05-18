bd32b4959af3fe1153d4495347565a4fabcabe66
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-05-18 22:07:25 +0800

    Update anagram.py

--- anagram.py
+++ anagram.py
@@ -1,17 +1,9 @@
-# your code goes here
-# your code goes here
 s=input()
 k="dhoni"
 if s==k:
 	print("no")
-elif len(s)==len(k):
-	for i in range(0,len(s)):
-		if k.count(s[i])!=1:
-			break
-	if i==len(s)-1:
-		print("yes")
-	else:
-		print("no")
+elif sorted(s)==sorted(k):
+	print("yes")
 else:
 	print("no")
- #anagram
+#anagram
