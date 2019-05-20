c5852ada8c956e5c2bdd3866f554b729ebbf6f2a
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-05-20 17:12:41 +0800

    Create pair_equal.py

---
+++ pair_equal.py
@@ -0,0 +1,14 @@
+# your code goes here
+n,m=map(int,input().split())
+l=list(map(int,input().split()))
+c=0
+for i in range(0,len(l)-1):
+	for j in range(i+1,len(l)):
+		if l[i]+l[j]==m:
+			c=c+1
+			break
+if c==0:
+	print("NO")
+else:
+	print("YES")
+#EQUAL TO M
