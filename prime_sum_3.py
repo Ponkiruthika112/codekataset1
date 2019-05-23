d2730bba48c379504acf9eff5fff9d27410ba125
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-05-24 00:43:16 +0800

    Create prime_sum_3.py

---
+++ prime_sum_3.py
@@ -0,0 +1,31 @@
+def prime(n):
+    l=[]
+    for k in range(2,n):
+        if k==2:
+            l.append(2)
+        else:
+            for i in range(2,k):
+                if k%i==0:
+                    break
+            if i==k-1:
+                l.append(k)
+    return l
+n=int(input())
+l=prime(n)
+print(l)
+c=0
+p=[]
+for i in range(0,len(l)):
+    for j in range(i,len(l)):
+        for k in range(j,len(l)):
+            if l[i]+l[j]+l[k]==n:
+                p.append([l[i],l[j],l[k]])
+if len(p)>1:
+    for i in p:
+        if len(list(set(i)))==len(i):
+            d=i
+else:
+    d=p[0]
+print(*d)
+    
+#prime
