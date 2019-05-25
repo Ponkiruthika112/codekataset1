856047698459ec97569a3f60ed9a73a2b7d2f824
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-05-25 10:38:50 +0800

    Create rev_at_odd.py

---
+++ rev_at_odd.py
@@ -0,0 +1,10 @@
+s=input()
+l=s.split()
+k=""
+for i in range(1,len(l)+1):
+    if i%2!=0:
+        k=k+l[i-1][::-1]+" "
+    else:
+        k=k+l[i-1]+" "
+print(k.strip())        
+#rev at odd pos
