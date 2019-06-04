3e3299d0b41d4f8662b85d9d291b1bc42c0c3625
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-06-04 23:04:45 +0800

    Create table.py

---
+++ table.py
@@ -0,0 +1,25 @@
+n=int(input())
+t=3
+c=3
+def check(n,p,t,j):
+	
+	for i in range(p+1,t+1):
+		if i==n:
+			k=j
+			break
+		j=j-1
+	return k
+if n<=3:
+	j=3
+	p=0
+	t=3
+	print(check(n,p,t,j))
+	
+else:
+	while t<n:
+		c=2*c
+		p=t
+		t=t+c
+	j=c
+	print(check(n,p,t,j))
+  #jm
