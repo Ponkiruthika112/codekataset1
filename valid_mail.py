fd4fc94ec7ed97badb5540cad6161f861995c195
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-06-11 01:57:39 +0800

    Create valid_mail.py

---
+++ valid_mail.py
@@ -0,0 +1,18 @@
+s=input()
+c=0
+if s.count("@")==1 and s.count(".")==1:
+        c=c+1
+for i in range(0,len(s)):
+        if s[i]=="@":
+                k=i
+        if s[i]==".":
+                p=i-k-1
+if k>=3 and p>=4:
+        c=c+1
+if s[len(s)-4::]==".com":
+        c=c+1
+if c==3:
+        print("yes")
+else:
+        print("no")
+#email
