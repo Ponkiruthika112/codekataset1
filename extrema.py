77fa4abf8c8a70ae5bf7409bde36b8c3603770da
Ponkiruthika112 <46352265+Ponkiruthika112@users.noreply.github.com>
2019-06-22 17:52:38 +0800

    Create extrema.py

---
+++ extrema.py
@@ -0,0 +1,11 @@
+n=int(input())
+l=list(map(int,input().split()))
+c=0
+for i in range(1,len(l)-1):
+    if l[i]>l[i-1] and l[i]>l[i+1] or l[i]<l[i-1] and l[i]<l[i+1]:
+        c=c+1
+if len(l)==1:
+    print(1)
+else:
+    print(c)
+#d
